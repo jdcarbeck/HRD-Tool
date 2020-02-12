c934ab34defe22f4bac71b8d28c14d2a31249611
jdcarbeck <jdcarbeck@gmail.com>
2020-02-12 10:37:53 +0000

    added nav_graph and updated dependencies

--- app/src/main/java/com/example/hrdtool/MainActivity.java
+++ app/src/main/java/com/example/hrdtool/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;

cae1c797ce56ffe510db29615133a60eb1942cac
bharrisotcd <36005478+bharrisotcd@users.noreply.github.com>
2020-03-22 19:20:01 +0000

    Update OkHttp.java

--- AndriodApp/app/src/main/java/com/example/hrdtool/OkHttp.java
+++ AndriodApp/app/src/main/java/com/example/hrdtool/OkHttp.java
@@ -2,8 +2,10 @@ package com.example.hrdtool;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Base64;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.PublicKey;
 import java.util.concurrent.CountDownLatch;
 
@@ -23,18 +25,17 @@ public class OkHttp {
 
     public static void sendPostReq(String... parameters)  {
 
-
+        final String paramOne = parameters[1];
         RequestBody body = RequestBody.create(
                 MediaType.parse("charset=utf-8"),
                 parameters[1] + parameters[2]
         );
 
         Request request = new Request.Builder()
-                .url(http://(LocalIpAddress:3000/HRform-router/" + parameters[0])
+                .url("http://192.168.0.11:3000/HRform-router/" + parameters[0])
                 .addHeader("User-Agent", "OkHttp Bot")
                 .post(body)
                 .build();
-
 
         httpClient.newCall(request).enqueue(new Callback() {
             @Override
@@ -48,11 +49,14 @@ public class OkHttp {
                     throw new IOException("Unexpected code " + response);
                 } else {
                    String responseBody = response.body().string();
-                   MainActivity.setPublicKey(responseBody);
-                   System.out.println((responseBody));
+                    if (paramOne.equals("")){
+                        MainActivity.setPublicKey(responseBody);
+                        System.out.println((responseBody));
+                    }
+
                 }
             }
-            
+
         });
 
     }

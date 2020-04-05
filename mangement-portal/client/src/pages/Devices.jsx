90aa78c149857cf548924ef296b16a821ad8b5d1
jdcarbeck <jdcarbeck@gmail.com>
2020-04-05 09:57:58 +0100

    changes to the device screen

--- mangement-portal/client/src/pages/Devices.jsx
+++ mangement-portal/client/src/pages/Devices.jsx
@@ -46,46 +46,7 @@ const Actions = styled.div`
 class Devices extends Component {
     render() {
         return (
-            <Popup trigger={<button className="btn btn-outline-success"> Open Modal </button>} modal>
-                {close => (
-                <Modal>
-                    <Header> Modal Title </Header>
-                    <Content>
-                    {" "}
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-                    Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-                    delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-                    <br />
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-                    commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-                    explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
-                    </Content>
-                    <Actions>
-                    <Popup
-                        trigger={<button className="btn btn-outline-primary"> Trigger </button>}
-                        position="top center"
-                        closeOnDocumentClick
-                    >
-                        <span>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-                        magni omnis delectus nemo, maxime molestiae dolorem numquam
-                        mollitia, voluptate ea, accusamus excepturi deleniti ratione
-                        sapiente! Laudantium, aperiam doloribus. Odit, aut.
-                        </span>
-                    </Popup>
-                    <button
-                        className="btn btn-outline-secondary"
-                        onClick={() => {
-                        console.log("modal closed ");
-                        close();
-                        }}
-                    >
-                        close
-                    </button>
-                    </Actions>
-                </Modal>
-                )}
-            </Popup>
+            <span>This is the device mangement screen</span>
         )
     }
 }
